83ee6d1f70197a6204b2f0578066e7f90eedb945
shaktigurung <mr.shakti.gurung@gmail.com>
2019-05-31 13:01:42 +1000

    apollo initialize

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './../styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          <h1> Hacker News </h1>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
